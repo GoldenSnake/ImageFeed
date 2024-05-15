17e704398c2d7d3384631faddbee2005bfe83fd6
Elinor <39811460+GoldenSnake@users.noreply.github.com>
2024-05-15 23:36:43 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Elina Shterengarts on 15.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
