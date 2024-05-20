a1bafa4dc60bfdbf94fb6a812d7c133b4cb88cf7
GoldenSnake <elst1995@gmail.com>
2024-05-20 19:46:04 +0300

    Добавлены Delegate и DataSource

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -19,3 +19,16 @@ class ImagesListViewController: UIViewController {
 
 }
 
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+}
