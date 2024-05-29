bcb8377cd3cf39d68a777fddb5544f416ce30373
GoldenSnake <elst1995@gmail.com>
2024-05-29 19:09:49 +0300

    Создан SingleImageView

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+}
