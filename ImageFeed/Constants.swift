64c837f3be901882823c7511df448e955aa45e62
GoldenSnake <elst1995@gmail.com>
2024-06-08 00:35:39 +0300

    Добавлены Constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "wA174RjshDxdTQ0K6tcEGz9IRZko_PMwiwSPDp5ZCJA"
+    static let secretKey = "deLLJ9DgAAabTTKD8WvHUp1js34Hk9NBmjqh1cfIr1U"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
