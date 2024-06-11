e8c4cf0986cd7b1ba0fe4056fb62c7c046376c62
GoldenSnake <elst1995@gmail.com>
2024-06-12 02:31:25 +0300

    URLComponents

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -4,16 +4,35 @@
 //
 
 import UIKit
-import WebKit 
+import WebKit
 
 class WebViewViewController: UIViewController {
-
+    
     @IBOutlet private var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        loadWebView()
     }
+}
 
-    
+// MARK: - loadWebView
+
+private extension WebViewViewController {
+    func loadWebView() {
+        var urlComponents = URLComponents(string: "https://unsplash.com/oauth/authorize")
+        urlComponents?.queryItems = [URLQueryItem(name: "client_id", value: Constants.accessKey),
+                                     URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+                                     URLQueryItem(name: "response_type", value: "code"),
+                                     URLQueryItem(name: "scope", value: Constants.accessScope)]
+        
+        if let url = urlComponents?.url{
+            let request = URLRequest(url: url)
+            webView.load(request)
+        } else {
+            print("Error")
+            return
+        }
+    }
 }
