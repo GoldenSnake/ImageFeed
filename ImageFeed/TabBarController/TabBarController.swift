19a8d30827bd066f4de427d5b9e7217d4b0eb64c
GoldenSnake <elst1995@gmail.com>
2024-08-26 18:50:06 +0300

    Вёрстка кодом. Profile View Controller

---
+++ ImageFeed/TabBarController/TabBarController.swift
@@ -0,0 +1,27 @@
+//
+//  TabBarController.swift
+//  ImageFeed
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        
+        let imagesListViewController = storyboard.instantiateViewController(
+            withIdentifier: "ImagesListViewController"
+        )
+        
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(named: "tab_profile_active"),
+            selectedImage: nil
+        )
+        
+        self.viewControllers = [imagesListViewController, profileViewController]
+    }
+    
+}
